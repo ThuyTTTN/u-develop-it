2e6f35fee6ddec2656612bb5284ac0df8287535f
Thuy Nguyen <ttruong06@gmail.com>
2022-05-06 12:15:55 -0400

    modified schema/seeds sql and server.js

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ const db = mysql.createConnection(
 
 //Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -52,7 +56,12 @@ app.get("/api/candidates", (req, res) => {
 
 //Get a Single candidate - create the API endpoint to get a single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -127,12 +136,12 @@ db.query(sql, params, (err, result) => {
 });
 
 
-app.get("/", (req, res) => {
-  res.json({
-    //send response msg back to the client
-    message: "Hello world",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.json({
+//     //send response msg back to the client
+//     message: "Hello world",
+//   });
+// });
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
